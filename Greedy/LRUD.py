6e4014d561c0965f2feee32f18ffb1bd8abf7a06
신 우현 <woo_hyun_1@naver.com>
2022-10-05 16:54:51 +0900

    LRUD

---
+++ Greedy/LRUD.py
@@ -0,0 +1,17 @@
+n = int(input())
+plans = input().split()
+
+x = 1
+y = 1
+
+for i in plans:
+    if (i == "L" and y > 1) :
+        y -= 1
+    elif (i == "R" and y < n):
+        y += 1
+    elif (i == "U" and x > 1):
+        x -= 1
+    elif (i == "D" and x < n):
+        x += 1        
+
+print(x,y)

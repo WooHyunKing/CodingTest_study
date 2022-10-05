0d143207fe7a1ee67dd7a4ec137ff3db34c7611b
신 우현 <woo_hyun_1@naver.com>
2022-10-05 17:19:01 +0900

    Time

---
+++ Implementation/Time.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+count = 0
+
+for time in range(n+1):
+    for min in range(60):
+        for sec in range(60):
+            if '3' in str(time)+str(min)+str(sec):
+                count+=1
+
+print(count)
+

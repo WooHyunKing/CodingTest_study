b33c5b66a20e5a6e633549b8204bf8a5148236b2
신 우현 <woo_hyun_1@naver.com>
2022-11-10 17:21:27 +0900

    baekjoon - 1260

--- BFS/bfs_example.py
+++ BFS/bfs_example.py
@@ -1,20 +1,35 @@
 from collections import deque
 
 # BFS 메소드 정의
-def bfs(graph,start,visited):
-    # Queue 구현을 위해 deque 라이브러리 사용
-    queue = deque([start])
+# def bfs(graph,start,visited):
+#     # Queue 구현을 위해 deque 라이브러리 사용
+#     queue = deque([start])
     
-    # 현재 노드를 방문 처리
+#     # 현재 노드를 방문 처리
+#     visited[start]=True
+
+#     # 큐가 빌 때까지 반복
+#     while queue:
+#         # 큐에서 하나의 원소를 뽑아서 출력
+#         v = queue.popleft()
+#         print(v,end=' ')
+
+#         # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+#         for i in graph[v]:
+#             if not visited[i]:
+#                 queue.append(i)
+#                 visited[i]=True
+
+def bfs(graph,start,visited):
+    # [ [1], [] , [] , [] ,[ ], [], ]
+    queue = deque([start])
+
     visited[start]=True
 
-    # 큐가 빌 때까지 반복
     while queue:
-        # 큐에서 하나의 원소를 뽑아서 출력
         v = queue.popleft()
         print(v,end=' ')
-
-        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
--- Implementation/String_compression.py
+++ Implementation/String_compression.py
@@ -6,34 +6,31 @@ length_list=[]
 
 for length in range(1,str_length+1):
 
-    temp_length = str_length
+    temp_length = str_length # 문자열 길이 temp에 임시 저장
 
-    result = [s[j:j+length] for j in range(0,len(s),length)]
-
-    # print(result)
+    result = [s[j:j+length] for j in range(0,len(s),length)] # 개수 단위로 문자열 잘라서 리스트로 저장
 
     count = 1
 
     for i in range(len(result)):
         
         if i == (len(result)-1) :
-            if count>1:
+            if count > 1:
                 temp_length -= (length * count)
                 temp_length += (length + len(str(count)))
                 count = 1
             break
             
         if result[i] == result[i+1]: # 다음 문자열과 동일한 경우
-            count+=1
+            count += 1
         elif count>1: # 동일하지 않은 경우
             temp_length -= (length * count)
             temp_length += (length + len(str(count)))
             count = 1
-    # print("length is ", length)
-    # print(temp_length)
-    # print("")
+
     length_list.append(temp_length)
 
 
-print(min(length_list))
+print(min(length_list)) # 최소의 길이 출력
+
 # length * 반복 횟수 => length + 1

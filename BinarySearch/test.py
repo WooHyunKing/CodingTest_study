df9e2aaf0930c50a16a6da584e25614cd2061c9e
신 우현 <woo_hyun_1@naver.com>
2023-01-29 00:07:04 +0900

    Resolve of BigNumberRule

---
+++ BinarySearch/test.py
@@ -0,0 +1,11 @@
+a = {1,2,3,4,5}
+b= {3,4,5,6,7}
+
+a.add(6)
+print(a)
+
+b.update({8,9})
+print(b)
+
+a.remove(6)
+print(a)
--- Greedy/BigNumberRule.py
+++ Greedy/BigNumberRule.py
@@ -1,36 +1,39 @@
 import time
 
-# 모범답안
-# N, M, K를 공백으로 구분하여 입력받기
-n,m,k = map(int,input().split())
+# # 모범답안
+# # N, M, K를 공백으로 구분하여 입력받기
+# n,m,k = map(int,input().split())
 
-# N개의 수를 공백으로 구분하여 입력받기
-data = list(map(int,input().split()))
+# # N개의 수를 공백으로 구분하여 입력받기
+# data = list(map(int,input().split()))
 
-total = 0
+# total = 0
 
-data.sort(reverse=True)
+# data.sort(reverse=True)
 
-start_time = time.time()
+# start_time = time.time()
 
-count = int(m/(k+1))*k
-count += m%(k+1)
+# count = int(m/(k+1))*k
+# count += m%(k+1)
 
-total += count*data[0]
-total += (m-count)*data[1]
+# total += count*data[0]
+# total += (m-count)*data[1]
 
-end_time = time.time()
+# end_time = time.time()
 
-print("time : ",end_time-start_time)
+# print("time : ",end_time-start_time)
 
-print(total)
+# print(total)
+
 
 # 본인 풀이
-# while m>0:
-#     new_k = min(m,k)
-#     total += (data[0] * new_k)
-#     m -= new_k
+n, m, k = map(int,input().split());
+arr = list(map(int,input().split()));
 
-#     if(m>0):
-#         total += data[1]
-#         m -= 1
+arr.sort(reverse=True);
+
+a = m // (k+1);
+b = m % (k+1);
+
+total = ((arr[0]*k)+arr[1])*a + (b*arr[0]);
+print(total);

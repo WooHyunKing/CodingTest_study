f473f1ee64479f1985fc01891a4bbe5b4f4a200a
신 우현 <woo_hyun_1@naver.com>
2023-01-29 12:30:27 +0900

    Resolve of FlipString

--- Greedy/FlipString.py
+++ Greedy/FlipString.py
@@ -1,21 +1,41 @@
-data = input()
+# data = input()
+
+# zero_count = 0
+# one_count = 0
+
+# current = data[0]
+
+# if(data[0]=="1"):
+#     one_count+=1
+# else:
+#     zero_count+=1
+
+# for i in range(1, len(data)):
+#     if(data[i] != current):
+#         if(data[i]=="1"):
+#             one_count+=1
+#         else:
+#             zero_count+=1
+#     current=data[i]
+
+# print(min(zero_count,one_count))
+
+data = list(map(int,input()))
 
 zero_count = 0
 one_count = 0
 
-current = data[0]
+zero_first = True
+one_first = True
 
-if(data[0]=="1"):
-    one_count+=1
-else:
-    zero_count+=1
-
-for i in range(1, len(data)):
-    if(data[i] != current):
-        if(data[i]=="1"):
-            one_count+=1
-        else:
-            zero_count+=1
-    current=data[i]
+for i in range(len(data)):
+    if((data[i] == 0) and (zero_first == True) ):
+        zero_count += 1
+        zero_first = False
+        one_first = True
+    elif((data[i] == 1) and (one_first == True)):
+        one_count += 1
+        one_first = False
+        zero_first = True
 
 print(min(zero_count,one_count))

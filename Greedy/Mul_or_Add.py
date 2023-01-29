8be8a694b69d71b68912b256a2a225286eed8f1a
신 우현 <woo_hyun_1@naver.com>
2023-01-29 11:47:30 +0900

    Resolve of MulOrAdd

--- Greedy/Mul_or_Add.py
+++ Greedy/Mul_or_Add.py
@@ -1,27 +1,9 @@
-import time
+# import time
 
-number = list(map(int,input())) # 입력한 문자열을 정수 리스트로 변환
+# start_time = time.time()
 
-start_time = time.time()
-
-result = 0
-
-for i in range(len(number)-1): # 문자열의 길이 만큼 반복
-    if number[i] <= 1 or number[i+1] <= 1: # 처리할 두 숫자 중 하나라도 값이 0이거나 1이라면
-        result = number[i]+number[i+1] # 더하기 연산
-        number[i+1]=result # 연산결과 저장
-    else:
-        result = number[i]*number[i+1] # 처리할 두 숫자 모두 2이상 이라면 곱하기 연산
-        number[i+1]=result # 연산결과 저장
-
-end_time = time.time()
-
-print(end_time-start_time)
-
-print(result)
-
+# 교재풀이 : 5.888938903808594e-05
 # data = input()
-
 # result=int(data[0])
 
 # start_time = time.time()
@@ -32,9 +14,24 @@ print(result)
 #         result += num
 #     else :
 #         result *= num
+# print(result)
+
+###################################
+
+# 본인 풀이 : 1.9759619235992432
+data = list(map(int,input()))
+
+total = 0
+
+for i in range(len(data)):
+    if((total <= 1) or (data[i] <= 1)):
+        total += data[i]
+    else:
+        total *= data[i]
+
+print(total)
+
 
 # end_time = time.time()
 
 # print(end_time-start_time)
-
-# print(result)

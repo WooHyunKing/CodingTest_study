91c4e5af2b5fd1d59399ce0247c2005cb5e82ef0
신 우현 <woo_hyun_1@naver.com>
2023-02-11 17:12:42 +0900

    Resolve BFS maze

--- DFS_BFS/maze.py
+++ DFS_BFS/maze.py
@@ -1,36 +1,71 @@
 from collections import deque
 
-n,m=map(int,input().split())
+# n,m=map(int,input().split())
 
-graph=[]
+# graph=[]
+
+# for i in range(n):
+#     graph.append(list(map(int,input())))
+
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
+
+# def bfs(x,y):
+#     queue=deque()
+#     queue.append((x,y))
+    
+#     while queue:
+#         x,y = queue.popleft()
+
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+
+#             if nx<0 or ny<0 or nx >= n or ny >= m:
+#                 continue
+
+#             if graph[nx][ny]==0:
+#                 continue
+
+#             if graph[nx][ny]==1:
+#                 graph[nx][ny]=graph[x][y]+1
+#                 queue.append((nx,ny))
+#                 print(graph[nx][ny])
+#     return graph[n-1][m-1]
+
+# print(bfs(0,0))
+
+n, m = map(int,input().split())
+
+maze = []
 
 for i in range(n):
-    graph.append(list(map(int,input())))
+    maze.append(list(map(int,input())))
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 def bfs(x,y):
-    queue=deque()
+    queue = deque()
     queue.append((x,y))
-    
+
     while queue:
         x,y = queue.popleft()
-
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx<0 or ny<0 or nx >= n or ny >= m:
+            if nx < 0 or nx >=n or ny<0 or ny>=m:
                 continue
 
-            if graph[nx][ny]==0:
-                continue
-
-            if graph[nx][ny]==1:
-                graph[nx][ny]=graph[x][y]+1
+            if maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y] + 1
                 queue.append((nx,ny))
-                print(graph[nx][ny])
-    return graph[n-1][m-1]
+
+        print(maze)
+
+    return maze[n-1][m-1]
 
 print(bfs(0,0))
+

e039329277ccf992e2099c647faa1ae21f4442e8
신 우현 <woo_hyun_1@naver.com>
2023-02-20 20:51:54 +0900

    Resolve TwoArrayChange

--- Sorting/TwoArrayChange.py
+++ Sorting/TwoArrayChange.py
@@ -1,16 +1,30 @@
-n,k = list(map(int, input().split()))
+# n,k = list(map(int, input().split()))
 
 
-arr1 = list(map(int,input().split()))
-arr2 = list(map(int,input().split()))
+# arr1 = list(map(int,input().split()))
+# arr2 = list(map(int,input().split()))
 
-arr1.sort()
-arr2.sort(reverse=True)
+# arr1.sort()
+# arr2.sort(reverse=True)
+
+# for i in range(k):
+#     if arr1[i] < arr2[i]:
+#         arr1[i],arr2[i] = arr2[i], arr1[i]
+#     else:
+#         break
+
+# print(sum(arr1))
+
+n, k = map(int,input().split())
+
+arr_a = list(map(int,input().split()))
+arr_b = list(map(int,input().split()))
+
+arr_a.sort()
+arr_b.sort(reverse=True)
 
 for i in range(k):
-    if arr1[i] < arr2[i]:
-        arr1[i],arr2[i] = arr2[i], arr1[i]
-    else:
-        break
+    if arr_a[i] < arr_b[i]:
+        arr_a[i],arr_b[i] = arr_b[i],arr_a[i]
 
-print(sum(arr1))
+print(sum(arr_a))

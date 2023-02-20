9fb3f747ab0bc4892cb557de3f4e2845a72fe32d
신 우현 <woo_hyun_1@naver.com>
2023-02-20 20:10:10 +0900

    Resolve LowerScore

--- Sorting/LowerScore.py
+++ Sorting/LowerScore.py
@@ -1,14 +1,27 @@
+# n = int(input())
+
+# List = []
+
+# for i in range(n):
+#     input_data = input().split()
+#     List.append((input_data[0], int(input_data[1])))
+
+# List.sort(key=lambda x:x[1])
+
+# # print(List)
+
+# for stuent in List:
+#     print(stuent[0], end=' ')
+
 n = int(input())
 
-List = []
+arr = []
 
 for i in range(n):
-    input_data = input().split()
-    List.append((input_data[0], int(input_data[1])))
+    data = tuple(input().split())
+    arr.append(data)
 
-List.sort(key=lambda x:x[1])
+arr.sort(key=lambda x:x[1])
 
-# print(List)
-
-for stuent in List:
-    print(stuent[0], end=' ')
+for i in arr:
+    print(i[0],end=' ')

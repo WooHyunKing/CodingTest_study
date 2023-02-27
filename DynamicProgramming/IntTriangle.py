32c4b0e84ec6a3cf9b3cf708f3b48b6cd67fd41a
신 우현 <woo_hyun_1@naver.com>
2023-02-27 23:28:39 +0900

    Resolve programmers int-triangle

--- DynamicProgramming/IntTriangle.py
+++ DynamicProgramming/IntTriangle.py
@@ -1,15 +1,20 @@
-# n = int(input())
-
-# table = [[0]*(n+1) for _ in range(n+1)]
-
-# result = [0]*(n*n)
-
-# for i in range(n):
-#     for j in range(i):
-#         table[i][j] = 
-
-
-
-
-# for i in range(n):
+def solution(triangle):
     
+    height = len(triangle) # 삼각형의 높이
+    
+    dp_list = [[0]*height for _ in range(height)]
+    
+    dp_list[0][0] = triangle[0][0]
+    
+    for i in range(1,height):
+        for j in range(i):
+            if (i-1)>=0 and (j-1)>=0:
+                dp_list[i][j] = max(dp_list[i-1][j-1]+triangle[i][j], dp_list[i-1][j]+triangle[i][j])
+            elif (i-1)>=0 and (j-1)<0:
+                dp_list[i][j] = dp_list[i-1][j] + triangle[i][j]
+            elif (i-1)<j and (j-1)>=0:
+                dp_list[i][j] = dp_list[i-1][j-1] + triangle[i][j]    
+            
+    max_value = max(dp_list[height-1])
+
+    return max_value

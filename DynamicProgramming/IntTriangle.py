d4b50a12d3839ef989cb2a78ce3463ab6e846c81
신 우현 <woo_hyun_1@naver.com>
2023-02-27 23:53:55 +0900

    Final solution of int-triangle

--- DynamicProgramming/IntTriangle.py
+++ DynamicProgramming/IntTriangle.py
@@ -7,14 +7,15 @@ def solution(triangle):
     dp_list[0][0] = triangle[0][0]
     
     for i in range(1,height):
-        for j in range(i):
-            if (i-1)>=0 and (j-1)>=0:
-                dp_list[i][j] = max(dp_list[i-1][j-1]+triangle[i][j], dp_list[i-1][j]+triangle[i][j])
-            elif (i-1)>=0 and (j-1)<0:
-                dp_list[i][j] = dp_list[i-1][j] + triangle[i][j]
-            elif (i-1)<j and (j-1)>=0:
-                dp_list[i][j] = dp_list[i-1][j-1] + triangle[i][j]    
+        for j in range(len(triangle[i])):
+            if j==0:
+                dp_list[i][j] = dp_list[i-1][0] + triangle[i][0]
+            elif i==j:
+                dp_list[i][j] = dp_list[i-1][j-1] + triangle[i][j] 
             
-    max_value = max(dp_list[height-1])
+            else:
+                dp_list[i][j] = max(dp_list[i-1][j-1]+triangle[i][j], dp_list[i-1][j]+triangle[i][j])
+               
+    max_value = max(dp_list[-1])
 
     return max_value

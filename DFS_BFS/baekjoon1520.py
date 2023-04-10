c5b8be387bd968d6c3428201b55d36334fc2e1cd
신 우현 <woo_hyun_1@naver.com>
2023-04-10 13:58:24 +0900

    Solved baekjoon 1520

--- DFS_BFS/baekjoon1520.py
+++ DFS_BFS/baekjoon1520.py
@@ -2,41 +2,30 @@ import sys
 sys.setrecursionlimit(50000000);
 
 m, n = map(int,input().split())
-
-area = [[] for _ in range(m)]
-visited = [[False]*n for _ in range(m)]
-
-count = 0
-
-for i in range(m):
-    area[i] = list(map(int,input().split()))
+area = [list(map(int,input().split())) for _ in range(m)]
+dp = [[-1]*n for _ in range(m)]
+nx, ny = [-1,1,0,0], [0,0,-1,1]
 
 def dfs(x,y):
-
-    global count
-    global visited
-
-    if visited[x][y]:
-        return False
     
+    # 도착 지점에 도달하면 1 리턴
     if x == (m-1) and y == (n-1):
-        count += 1
+        return 1
+    # 이미 방문한 길이라면 경우의 수를 구하지 않고 해당 위치에서 출발하는 경우의 수 리턴
+    if dp[x][y] != -1:
+        return dp[x][y]
     
-    visited[x][y] = True
+    way_count = 0
 
-    nx = [-1,1,0]
-    ny = [0,0,1]
-
-    for i in range(3):
+    for i in range(4):
         temp_x = x + nx[i]
         temp_y = y + ny[i]
 
-        if temp_x >= 0 and temp_x < m and temp_y >= 0 and temp_y < n:
-            if area[temp_x][temp_y] < area[x][y]:
-                dfs(temp_x,temp_y)
+        if temp_x >= 0 and temp_x < m and temp_y >= 0 and temp_y < n and area[temp_x][temp_y] < area[x][y]:
+                way_count += dfs(temp_x,temp_y)
+    
+    dp[x][y] = way_count
 
-    visited[x][y] = False
+    return dp[x][y]
 
-dfs(0,0)
-
-print(count)
+print(dfs(0,0))

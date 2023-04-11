d0e93a088fd16270895cb9d8178c82f59050e1c2
신 우현 <woo_hyun_1@naver.com>
2023-04-11 14:10:15 +0900

    Solved baekjoon 2293

--- DynamicProgramming/baekjoon2293.py
+++ DynamicProgramming/baekjoon2293.py
@@ -1,21 +1,41 @@
 n, k = map(int,input().split())
 
-price = [0] * (n+1)
+# 1차원 DP 테이블을 사용한 풀이
+prices = [0] * n
 
-for i in range(1,n+1):
-    price[i] = int(input())
+for i in range(n):
+    prices[i] = int(input())
 
-dp = [[0]*(k+1) for _ in range(n+1)]
+# dp[i] -> i원을 만들 때 가능한 경우의 수
+dp = [0 for _ in range(k+1)]
+# dp[0] -> 0원을 만들 때 가능한 경우의 수, 동전을 사용하지 않는 경우 이므로 1로 초기화
+dp[0] = 1
 
-for i in range(n+1):
-     for j in range(k+1):
-          if j == 0:
-               dp[i][j]= 1 
+for price in prices:
+    for i in range(price,k+1):
+        dp[i] = dp[i] + dp[i-price]
+        
+print(dp[k])
 
-for i in range(1,n+1):
-    for j in range(1,k+1):
-            if j >= price[i]:
-                dp[price[i]][j] = dp[price[i]-1][j] + dp[price[i]][j-price[i]]
-            else:
-                 dp[price[i]][j] = dp[price[i]-1][j]
-print(dp)
+# 2차원 DP 테이블를 사용한 풀이(시간초과)
+
+# prices = [0] * (n+1)
+
+# for i in range(1,n+1):
+#     prices[i] = int(input())
+
+# dp = [[0]*(k+1) for _ in range(n+1)]
+
+# for i in range(1,n+1):
+#     for j in range(k+1):
+#         if j==0:
+#             dp[i][j] = 1
+
+# for i in range(1,n+1):
+#     for j in range(1,k+1):
+#         if prices[i] > j :
+#             dp[i][j] = dp[i-1][j]
+#         else:
+#             dp[i][j] = dp[i-1][j] + dp[i][j-prices[i]]
+
+# print(dp[n][k])

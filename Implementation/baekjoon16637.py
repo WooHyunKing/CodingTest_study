bcda7df97d4648dada588bd847616d7a3285e6ed
신 우현 <woo_hyun_1@naver.com>
2023-08-10 20:47:43 +0900

    Change variable name baekjoon - 16637

--- Implementation/baekjoon16637.py
+++ Implementation/baekjoon16637.py
@@ -1,11 +1,8 @@
 # 우선순위 X, 중첩괄호 X, 괄호 없어도 Ok
 # 괄호 안에는 연산자 1개만 가능
-
 # 괄호를 적절히 추가해서 최대값 만들기
 
 n = int(input())
-
-#['3', '+', '8', '*', '7', '-', '9', '*', '2']
 formula = list(input())
 
 global max_value
@@ -19,19 +16,18 @@ def calculate(num1,operator,num2):
     elif operator == "*":
         return num1 * num2
 
-def compare(index,value):
+def dfs(index,value):
     global max_value
-    print("value : ",value)
 
     if index == n - 1:
         max_value = max(max_value,value)
         return
 
     if index + 2 < n:
-        compare(index+2, calculate(value, formula[index+1], int(formula[index+2])))
+        dfs(index+2, calculate(value, formula[index+1], int(formula[index+2])))
     if index + 4 < n:
-        compare(index+4, calculate(value, formula[index+1], calculate(int(formula[index+2]), formula[index+3], int(formula[index+4]) )) )
+        dfs(index+4, calculate(value, formula[index+1], calculate(int(formula[index+2]), formula[index+3], int(formula[index+4]))))
 
-compare(0,int(formula[0]))
+dfs(0,int(formula[0]))
 
 print(max_value)

579b2ea5e17f4064fd0646ba79cc1e25c0053259
신 우현 <woo_hyun_1@naver.com>
2023-08-25 17:38:26 +0900

    Optimize baekjoon - 14500

--- Implementation/baekjoon14500.py
+++ Implementation/baekjoon14500.py
@@ -6,19 +6,23 @@ visited = [[False]*m for _ in range(n)]
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-max_value = -1
+answer = -1
+max_value = max(map(max,area))
+
 
 def dfs(x,y,movement,value):
 
-    global max_value
+    global answer
+
+    if answer >= value + area[x][y] + max_value*(4-movement):
+        return
+
+    if movement == 4:
+        answer = max(answer,value+area[x][y])
+        return
     
     visited[x][y] = True
 
-    if movement == 4:
-        max_value = max(max_value,value+area[x][y])
-        visited[x][y] = False
-        return
-    
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
 
@@ -28,20 +32,20 @@ def dfs(x,y,movement,value):
     visited[x][y] = False
 
 def other(x,y):
-    global max_value
+    global answer
 
     if 0 <= x-1 < n and 0 <= x+1 < n and 0 <= y+1 < m:
-        max_value = max(max_value, area[x-1][y]+area[x][y]+area[x+1][y]+area[x][y+1])
+        answer = max(answer, area[x-1][y]+area[x][y]+area[x+1][y]+area[x][y+1])
     if 0 <= x+1 < n and 0 <= y-1 < m and 0 <= y+1 < m:
-        max_value = max(max_value, area[x][y-1]+area[x][y]+area[x][y+1]+area[x+1][y])
+        answer = max(answer, area[x][y-1]+area[x][y]+area[x][y+1]+area[x+1][y])
     if 0 <= x-1 < n and 0 <= y-1 < m and 0 <= y+1 < m:
-        max_value = max(max_value, area[x][y-1]+area[x][y]+area[x][y+1]+area[x-1][y])
+        answer = max(answer, area[x][y-1]+area[x][y]+area[x][y+1]+area[x-1][y])
     if 0 <= x-1 < n and 0 <= x+1 < n and 0 <= y-1 < m:
-        max_value = max(max_value, area[x-1][y]+area[x][y]+area[x+1][y]+area[x][y-1])
+        answer = max(answer, area[x-1][y]+area[x][y]+area[x+1][y]+area[x][y-1])
 
 for i in range(n):
     for j in range(m):
         dfs(i,j,1,0)
         other(i,j)
 
-print(max_value)
+print(answer)

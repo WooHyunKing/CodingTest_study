f76815327e13776ba9e1526af4e0fdc6c535af1b
신 우현 <woo_hyun_1@naver.com>
2023-09-29 16:53:04 +0900

    Solved baekjoon - 17392

---
+++ Greedy/baekjoon17392.py
@@ -0,0 +1,32 @@
+n, m = map(int,input().split())
+
+h = list(map(int,input().split()))
+
+def getUnhappy(num):
+    total = 0
+    
+    for i in range(1,num+1):
+        total += i**2
+    
+    return total
+
+happy = sum(h) + len(h)
+unhappy = m - happy
+
+area = [0] * (n+1)
+
+index = 0
+
+for i in range(unhappy):
+    area[index] += 1
+    index += 1
+
+    if index >= n+1:
+        index = 0
+
+print(sum([getUnhappy(x) for x in area]))
+
+# if n >= unhappy:
+#     print(unhappy)
+# else:
+#     print((n) + getUnhappy(unhappy-(n)))

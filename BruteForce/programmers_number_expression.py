8756ca5a3754c2ab663cb947cd9a34a7cf59983c
신 우현 <woo_hyun_1@naver.com>
2024-01-05 18:03:00 +0900

    Solved programmers - number expression

---
+++ BruteForce/programmers_number_expression.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1,n+1):
+        sum_value = 0
+        for j in range(i,n+1):
+            sum_value += j
+            if sum_value == n:
+                answer += 1
+            elif sum_value > n:
+                break
+                
+    return answer

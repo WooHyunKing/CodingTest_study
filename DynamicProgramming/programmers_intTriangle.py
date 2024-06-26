a54bcf501505f48772e4f95a9cfc8082c298315a
신 우현 <woo_hyun_1@naver.com>
2024-06-27 07:26:05 +0900

    Resolved programmers - int triangle

---
+++ DynamicProgramming/programmers_intTriangle.py
@@ -0,0 +1,40 @@
+# def solution(triangle):
+    
+#     height = len(triangle) # 삼각형의 높이
+    
+#     dp_list = [[0]*height for _ in range(height)]
+    
+#     dp_list[0][0] = triangle[0][0]
+    
+#     for i in range(1,height):
+#         for j in range(len(triangle[i])):
+#             if j==0:
+#                 dp_list[i][j] = dp_list[i-1][0] + triangle[i][0]
+#             elif i==j:
+#                 dp_list[i][j] = dp_list[i-1][j-1] + triangle[i][j] 
+            
+#             else:
+#                 dp_list[i][j] = max(dp_list[i-1][j-1]+triangle[i][j], dp_list[i-1][j]+triangle[i][j])
+               
+#     max_value = max(dp_list[-1])
+
+#     return max_value
+
+def solution(triangle):
+    
+    n = len(triangle)
+    
+    dp = [[0]*n for _ in range(n)]
+    dp[0][0] = triangle[0][0]
+    
+    for i in range(1,n):
+        width = len(triangle[i])
+        for j in range(width):
+            if j == 0:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j == (width-1):
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+                
+    return max(dp[-1])

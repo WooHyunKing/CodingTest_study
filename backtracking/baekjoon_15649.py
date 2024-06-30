b06d6c289ead43eba3640a8af7fb62f7f980863d
신 우현 <woo_hyun_1@naver.com>
2024-06-30 19:09:47 +0900

    Resolved baekjoon - 15649

--- backtracking/baekjoon_15649.py
+++ backtracking/baekjoon_15649.py
@@ -1,25 +1,56 @@
+# n, m = map(int,input().split())
+
+# is_used = [False]*9
+
+# temp = []
+
+# answer = []
+
+# def backtracking():
+    
+#     if len(temp) == m:
+#         answer.append(' '.join(map(str,temp)))
+#         return
+#     for i in range(1,n+1):
+#         if not is_used[i]:
+#             is_used[i] = True
+#             temp.append(i)
+#             backtracking()
+#             is_used[i]= False
+#             temp.pop()
+
+# backtracking()   
+
+# for ans in answer:
+#     print(ans)
+
+import sys
+
+input = sys.stdin.readline
+
 n, m = map(int,input().split())
 
-is_used = [False]*9
-
-temp = []
+# 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
 
 answer = []
 
-def backtracking():
-    
-    if len(temp) == m:
-        answer.append(' '.join(map(str,temp)))
-        return
-    for i in range(1,n+1):
-        if not is_used[i]:
-            is_used[i] = True
-            temp.append(i)
-            backtracking()
-            is_used[i]= False
-            temp.pop()
+visited = [False]*(n+1)
 
-backtracking()   
+def dfs(numbers):
+    
+    if len(numbers) == m:
+        answer.append(numbers)
+        return
+    
+    for i in range(1,n+1):
+        if not visited[i]:
+            visited[i] = True
+            dfs(numbers+[i])
+            visited[i] = False
+
+dfs([])
 
 for ans in answer:
-    print(ans)
+    for n in ans:
+        print(n,end=" ")
+    print()

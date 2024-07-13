51f78846dda73ee931888f42ed2a37d7e821d27b
신 우현 <woo_hyun_1@naver.com>
2024-07-13 23:58:53 +0900

    Solving programmers - spicy

---
+++ Heap/programmers_spicy.py
@@ -0,0 +1,45 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    
+    # 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다
+    # 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
+    
+    # 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+    
+    # 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
+    
+    hq = []
+    
+    for data in scoville:
+        heapq.heappush(hq,data)
+    
+    while True:
+
+#         bool_list = [x for x in hq if x >= K]
+        
+#         if len(bool_list) == len(hq):
+#             break
+
+        if hq and len(hq) == 1 and hq[0] < k:
+            return -1
+            
+        if hq and hq[0] >= K:
+            break
+        
+        if hq:
+            a = heapq.heappop(hq)
+        else:
+            break
+            
+        if hq:
+            b = heapq.heappop(hq)
+        else:
+            break
+        
+        heapq.heappush(hq,a+b*2)
+        
+        answer += 1
+    
+    return answer

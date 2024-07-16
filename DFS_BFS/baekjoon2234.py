02bd2f7a6db89ad576b9ef02bbf9f966e17b06a7
신 우현 <woo_hyun_1@naver.com>
2024-07-17 08:57:01 +0900

    Solved baekjoon - 2234

---
+++ DFS_BFS/baekjoon2234.py
@@ -0,0 +1,98 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+# 서 : 1
+# 북 : 2
+# 동 : 4
+# 남 : 8
+
+# 굵은 선은 벽을 나타내고, 점선은 벽이 없어서 지나다닐 수 있는 통로
+
+# 1.성에 있는 방의 개수
+# 2.가장 넓은 방의 넓이
+# 3.하나의 벽을 제거하여 얻을 수 있는 가장 넓은 방의 크기
+
+# 성에는 최소 두 개의 방이 있어서, 항상 하나의 벽을 제거하여 두 방을 합치는 경우가 있다.
+
+dx, dy = [-1,1,0,0],[0,0,-1,1]
+
+n, m = map(int,input().split())
+
+visited = [[False]*n for _ in range(m)]
+
+area = [list(map(int,input().split())) for _ in range(m)]
+
+answer = []
+
+maximum = float("-inf")
+
+def get_block_info(n):
+
+    return list(bin(n).split('0b')[1][::-1].ljust(4,'0'))
+
+def bfs(x,y):
+
+    if visited[x][y]:
+        return -1
+    
+    visited[x][y] = True
+
+    q = deque([(x,y)])
+
+    count = 0
+
+    while q:
+        
+        cx, cy = q.popleft()
+
+        count += 1
+
+        if 0 <= cx-1 < m and not visited[cx-1][cy] and get_block_info(area[cx][cy])[1] == '0':
+            q.append((cx-1,cy))
+            visited[cx-1][cy] = True
+        if 0 <= cx+1 < m and not visited[cx+1][cy] and get_block_info(area[cx][cy])[3] == '0':
+            q.append((cx+1,cy))
+            visited[cx+1][cy] = True
+        if 0 <= cy-1 < n and not visited[cx][cy-1] and get_block_info(area[cx][cy])[0] == '0':
+            q.append((cx,cy-1)) 
+            visited[cx][cy-1] = True
+        if 0 <= cy+1 < n and not visited[cx][cy+1] and get_block_info(area[cx][cy])[2] == '0':
+            q.append((cx,cy+1))
+            visited[cx][cy+1] = True
+
+    return count
+
+for i in range(m):
+    for j in range(n):
+        result = bfs(i,j)
+        
+        if result != -1:
+            answer.append(result)
+
+for i in range(m):
+    for j in range(n):
+        block_info = get_block_info(area[i][j])
+
+        visited = [[False]*n for _ in range(m)]
+
+        if block_info[0] == '1':
+            area[i][j] -= 1
+            maximum = max(maximum,bfs(i,j))
+            area[i][j] += 1
+        if block_info[1] == '1':
+            area[i][j] -= 2
+            maximum = max(maximum,bfs(i,j))
+            area[i][j] += 2 
+        if block_info[2] == '1':
+            area[i][j] -= 4
+            maximum = max(maximum,bfs(i,j))
+            area[i][j] += 4
+        if block_info[3] == '1':
+            area[i][j] -= 8
+            maximum = max(maximum,bfs(i,j))
+            area[i][j] += 8
+
+print(len(answer))
+print(max(answer))
+print(maximum)
--- Graph/baekjoon1238.py
+++ Graph/baekjoon1238.py
@@ -20,38 +20,38 @@ for _ in range(m):
     start, end, t = map(int,input().split())
     graph[start].append((end,t))
 
-def dijkstra(start):    
+def dijkstra(start): # 출발 노드를 설정
 
-    distance = [INF]*(n+1)
+    distance = [INF]*(n+1) # 최단거리 테이블 초기화
 
-    distance[start] = 0
+    distance[start] = 0 # 출발 노드에서의 최단거리는 0
 
     q = []
 
     heapq.heappush(q,(0,start))
 
     while q:
-        d, current = heapq.heappop(q)
+        d, current = heapq.heappop(q) # 방문하지 않은 노드 중에서 가장 거리가 짧은 노드를 선택
         
-        if distance[current] < d:
+        if distance[current] < d: # 이미 선택된 노드는 Pass
             continue
 
-        for node, cost in graph[current]:
-            new_cost = d + cost
+        for node, cost in graph[current]: # 선택한 노드에서 이동할 수 있는 다른 노드와 비용
+            new_cost = d + cost # 새로운 거리 = 현재 노드까지의 최단거리 + 현재 노드에서 다음 노드까지의 거리
         
-            if new_cost < distance[node]:
-                distance[node] = new_cost
-                heapq.heappush(q,(new_cost,node))
+            if new_cost < distance[node]: # 새로운 거리가 다음 노드의 거리보다 짧을 경우
+                distance[node] = new_cost # 최단거리 갱신
+                heapq.heappush(q,(new_cost,node)) # 방문할 노드에 추가
 
-    return distance
+    return distance # 시작 노드로부터 다른 노드까지의 최단거리가 저장된 1차원 리스트 반환
 
-go = dijkstra(x)
+for i in range(1,n+1): # ( O(N * E * LogV) ) = 10^3 * 10^4 * log(10^3)
+    go = dijkstra(i) # 1번부터 N번 마을까지 각각 다른 마을까지의 최단거리 구하기 ( O(E * LogV) )
+    total_length[i] = go[x]
+
+back = dijkstra(x) # X 마을에서부터 다른 마을까지 돌아가는 최단거리 구하기
 
 for i in range(1,n+1):
-    back = dijkstra(i)
-    total_length[i] = back[x]
-
-for i in range(1,n+1):
-    total_length[i] += go[i]
+    total_length[i] += back[i] # 왕복거리를 구해야 하므로 X 마을에서부터 돌아가는 최단거리 더해주기
 
 print(max(total_length[1:]))

69ec1bfc85d8011c87784ba156043fe6bdfe80a6
신 우현 <woo_hyun_1@naver.com>
2024-07-29 08:07:05 +0900

    Resolved programmers - process

--- Queue/programmers_process.py
+++ Queue/programmers_process.py
@@ -1,18 +1,39 @@
+# def solution(priorities, location):
+#     answer = 0
+    
+#     queue = [(i,value) for i,value in enumerate(priorities)]
+    
+#     while queue:
+#         index, priority = queue.pop(0)
+        
+#         if queue and max([x[1] for x in queue]) > priority:
+#             queue.append((index,priority))
+#             continue
+        
+#         answer += 1
+        
+#         if index == location:
+#             return answer
+        
+#     return answer
+
+from collections import deque
+
 def solution(priorities, location):
     answer = 0
     
-    queue = [(i,value) for i,value in enumerate(priorities)]
+    sorted_values = sorted(priorities)
     
-    while queue:
-        index, priority = queue.pop(0)
+    priorities = deque([(x,i) for i,x in enumerate(priorities)])
+    
+    while True:
         
-        if queue and max([x[1] for x in queue]) > priority:
-            queue.append((index,priority))
-            continue
+        value, index = priorities.popleft()
         
-        answer += 1
-        
-        if index == location:
-            return answer
-        
-    return answer
+        if value != sorted_values[-1]:
+            priorities.append((value,index))
+        else:
+            sorted_values.pop()
+            answer += 1
+            if index == location:
+                return answer

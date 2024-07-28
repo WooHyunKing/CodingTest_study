afa894456c303f5f1d84a6ba24ce41a6200b6a12
신 우현 <woo_hyun_1@naver.com>
2024-07-29 08:26:08 +0900

    Resolved programmers - int triangle

--- DynamicProgramming/programmers_intTriangle.py
+++ DynamicProgramming/programmers_intTriangle.py
@@ -20,21 +20,45 @@
 
 #     return max_value
 
+# def solution(triangle):
+    
+#     n = len(triangle)
+    
+#     dp = [[0]*n for _ in range(n)]
+#     dp[0][0] = triangle[0][0]
+    
+#     for i in range(1,n):
+#         width = len(triangle[i])
+#         for j in range(width):
+#             if j == 0:
+#                 dp[i][j] = dp[i-1][j] + triangle[i][j]
+#             elif j == (width-1):
+#                 dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+#             else:
+#                 dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+                
+#     return max(dp[-1])
+
 def solution(triangle):
     
-    n = len(triangle)
+    length = len(triangle)
     
-    dp = [[0]*n for _ in range(n)]
-    dp[0][0] = triangle[0][0]
+    tri = dp = [[0]*(length) for _ in range(length)]
+    dp = [[0]*(length) for _ in range(length)]
     
-    for i in range(1,n):
-        width = len(triangle[i])
-        for j in range(width):
+    for i, row in enumerate(triangle):
+        for j, value in enumerate(row):
+            tri[i][j] = value
+    
+    dp[0][0] = tri[0][0]
+    
+    for i in range(1,length):
+        for j in range(i+1):
             if j == 0:
-                dp[i][j] = dp[i-1][j] + triangle[i][j]
-            elif j == (width-1):
-                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+                dp[i][j] = dp[i-1][j] + tri[i][j]
+            elif j == i:
+                dp[i][j] = dp[i-1][j-1] + tri[i][j]
             else:
-                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
-                
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + tri[i][j]
+    
     return max(dp[-1])

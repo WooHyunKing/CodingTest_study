c5d1d7af451babce62b93bee7159d11f42835b4f
신 우현 <woo_hyun_1@naver.com>
2024-08-01 07:41:58 +0900

    Learn about kruskal algorithm

---
+++ UnionFind/Kruskal.py
@@ -0,0 +1,40 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+
+parent = [0]*(v+1)
+
+# 모든 간선을 담을 리스트
+edges = []
+# 최종 비용을 담을 함수
+result = 0
+
+for i in range(1,v+1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    # 비용순으로 정렬하기 위해 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for cost, a, b in edges:
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)

1d5c80d773851a724e15a5dae3d8062eaef81ba4
신 우현 <woo_hyun_1@naver.com>
2024-10-01 14:39:29 +0900

    Implementation permutation by backtracking

--- permutation.py
+++ permutation.py
@@ -22,28 +22,54 @@
     
 #     return generate([],visited)
 
-def permutation(arr,r):
+# def permutation(arr,r):
 
-    visited = [False]*len(arr)
+#     visited = [False]*len(arr)
 
-    def generate(elements,visited):
+#     def generate(elements,visited):
 
-        if len(elements) == r:
-            return [elements[:]]
+#         if len(elements) == r:
+#             return [elements[:]]
         
-        cases = []
+#         cases = []
+
+#         for i in range(len(arr)):
+#             if not visited[i]:
+#                 elements.append(arr[i])
+#                 visited[i] = True
+#                 cases.extend(generate(elements,visited))
+#                 visited[i] = False
+#                 elements.pop()
+        
+#         return cases
+    
+#     return generate([],visited)
+        
+
+# print(permutation([1,2,3,4,5],3))
+
+def permutation(arr,k):
+    
+    visited = [False]*len(arr)
+    cases = []
+
+    def dfs(elements):
+
+        if len(elements) == k:
+            return cases.append(elements)
 
         for i in range(len(arr)):
             if not visited[i]:
-                elements.append(arr[i])
                 visited[i] = True
-                cases.extend(generate(elements,visited))
+                dfs(elements + [arr[i]])
                 visited[i] = False
-                elements.pop()
-        
-        return cases
     
-    return generate([],visited)
-        
+    dfs([])
 
-print(permutation([1,2,3,4,5],3))
+    return cases
+
+print(permutation([1,2,3,4],2))
+        
+    
+    
+    

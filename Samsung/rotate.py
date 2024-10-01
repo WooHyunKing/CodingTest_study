54f5358d5f049e1279fbb8116bdf495230f49c84
신 우현 <woo_hyun_1@naver.com>
2024-10-01 16:07:14 +0900

    Practice rotate 2-d array

--- Samsung/rotate.py
+++ Samsung/rotate.py
@@ -5,33 +5,37 @@ area = [[i * N + j for j in range(N)] for i in range(N)]
 for row in area:
     print(row)
 
+print()
+
 def rotate90_right():
     
     global area
     
     new_area = [[0]*N for _ in range(N)]
-
+    
     for i in range(N):
         for j in range(N):
-            new_area[j][N-1-i] = area[i][j]
+            new_area[j][N-i-1] = area[i][j]
 
-    area = new_area
+    return new_area
+
+area = rotate90_right()
+
+for row in area:
+    print(row)
+print()
 
 def rotate90_left():
-
-    global area
-
+    
     new_area = [[0]*N for _ in range(N)]
 
     for i in range(N):
         for j in range(N):
-            new_area[N-1-j][i] = area[i][j]
+            new_area[N-j-1][i] = area[i][j]
+    
+    return new_area
 
-    area = new_area
-
-rotate90_right()
-
-rotate90_left()
+area = rotate90_left()
 
 for row in area:
     print(row)

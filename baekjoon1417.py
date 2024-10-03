e491c94a07bcd66cb8fbe1a447ab8d55ab102c34
신 우현 <woo_hyun_1@naver.com>
2024-10-03 11:31:35 +0900

    Solved baekjoon - 1417

--- baekjoon1417.py
+++ baekjoon1417.py
@@ -1,38 +1,42 @@
 import sys
+import heapq
+
 input = sys.stdin.readline
 
-# {1, 2, ..., 49}에서 수 6개를 고른다
-# 49가지 수 중 k(k>6)개의 수를 골라 집합 S를 만든 다음 그 수만 가지고 번호를 선택하는 것
+# 국회의원 선거를 조작
+# 다솜이의 기계는 각 사람들이 누구를 찍을 지 미리 읽을 수 있다. 
+# 어떤 사람이 누구를 찍을 지 정했으면, 반드시 선거때 그 사람을 찍는다.
 
-def combinations(arr, k):
+# 형택구에 나온 국회의원 후보는 N명
+# 다솜이는 이 기계를 이용해서 그 마을의 주민 M명의 마음을 모두 읽었다.
 
-    cases = []
+# 다솜이는 기호 1번
+# 자신을 찍지 않으려는 사람을 돈으로 매수해서 국회의원에 당선이 되게 하려고 한다.
+# 다른 모든 사람의 득표수 보다 많은 득표수를 가질 때, 그 사람이 국회의원에 당선
 
-    def dfs(elements,index):
-        if len(elements) == k:
-            cases.append(elements)
-            return
-        for i in range(index+1,len(arr)):
-            dfs(elements+[arr[i]],i)
+# 출력 : 다솜이가 매수해야하는 사람의 최솟값
+
+N = int(input())
+
+my_vote = int(input())
+
+if N == 1:
+    print(0)
+    exit()
+
+hq = []
+
+answer = 0
+
+for _ in range(N-1):
+    heapq.heappush(hq,-int(input()))
+
+while my_vote <= -hq[0]:
     
-    dfs([],-1)
+    maximum_vote = -heapq.heappop(hq)
+    my_vote += 1
+    heapq.heappush(hq,-(maximum_vote-1))
+    answer += 1
 
-    return cases
 
-while True:
-
-    numbers = list(map(int,input().split()))
-
-    if len(numbers) == 1 and numbers[0] == 0:
-        break
-
-    k = numbers[0]
-    numbers = numbers[1:]
-
-    answer = sorted(combinations(numbers,6))
-    
-    for a in answer:
-        for b in a:
-            print(b, end=' ')
-        print()
-    print()
+print(answer)
